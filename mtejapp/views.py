c30b94320b22cc9d3ed3760d69d66fe5f8f9d555
Kevin Malomba <malombakevin@gmail.com>
2022-10-25 18:37:17 +0300

    feat: templates -Adds html templates

--- mtejapp/views.py
+++ mtejapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+
+    return render (request, 'index.html')
+
+
+def products(request):
+
+    return render (request, "products.html" )
+
+def customer(request):
+
+    return render (request, "customer.html" )
+   
