a72f99abf1bcc81e6db5b095c372b135b017076e
Kevin Malomba <malombakevin@gmail.com>
2022-10-28 14:10:43 +0300

    update

--- mtejapp/urls.py
+++ mtejapp/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns =[
     path('', views.home, name="home"),
     path('products/',views.products, name="products"),
-    path('customer/', views.customer, name="customer")
+    path('customer/<str:pk>', views.customer, name="customer"),
+    path('create_order', views.createOrder, name="create_order"),
+    path('update/<str:pk>', views.orderUpdate, name='update'),
+    path('deleteOrder/<str:pk>', views.deleteOrder, name="delete")
 
 
     ]
--- mtejapp/views.py
+++ mtejapp/views.py
@@ -1,18 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 # Create your views here.
 
 def home(request):
+    orders = Order.objects.all()
+    customers = Customer.objects.all()   
 
-    return render (request, 'index.html')
+    orders_total = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter( status='Pending').count()
+
+    context = {'orders':orders, 'customers': customers, 'ordersTotal':orders_total,'delivered':delivered, 'pending':pending}
+
+
+
+    return render (request, 'index.html', context)
 
 
 def products(request):
+    products = Product.objects.all()
 
-    return render (request, "products.html" )
+    context = {'products': products}
 
-def customer(request):
+    return render (request, "products.html", context)
 
-    return render (request, "customer.html" )
+def customer(request, pk):
+    
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    totalOrders = orders.count()
+
+    context= {'customer':customer, 'orders':orders, 'totalOrders': totalOrders}
+
+    return render (request, "customer.html", context )
+
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method=="POST":
+        # print("Printing:", request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context={'form':form}
+
+    return render(request, 'order_form.html', context)
+
+def orderUpdate(request, pk):
+    order = Order.objects.get(id=pk)
+
+    form = OrderForm(instance=order)
+
+    if request.method=="POST":
+        form = OrderForm(request.POST,instance=order )
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    context={'form':form}
+
+    return render (request, 'orderUpdate_form.html', context)
+
+def deleteOrder(request, pk):
+    order=Order.objects.get(id=pk)
+    form=OrderForm()
+    if request.method=="POST":
+        order.delete()
+        return redirect('/')
+
+    return render(request, 'deleteOrder.html')
+
+
    
